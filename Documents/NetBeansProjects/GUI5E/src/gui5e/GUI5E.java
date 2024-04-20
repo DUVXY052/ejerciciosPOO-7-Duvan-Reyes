11634ab223faf068f45e7b3409fc72b8ddf3d405
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 00:03:41 -0500

    friday

---
+++ Documents/NetBeansProjects/GUI5E/src/gui5e/GUI5E.java
@@ -0,0 +1,13 @@
+
+
+package gui5e;
+
+import gui5e.Clista;
+public class GUI5E {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Clista fact = new Clista();
+        fact.setVisible(true);
+    }
+}
