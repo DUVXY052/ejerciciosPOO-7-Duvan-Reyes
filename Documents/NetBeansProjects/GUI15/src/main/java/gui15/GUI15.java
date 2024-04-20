6d23b9d1202d9dfd4a1afe36e9403c77ddb22291
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 15:00:33 -0500

    saturday

---
+++ Documents/NetBeansProjects/GUI15/src/main/java/gui15/GUI15.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gui15;
+
+import gui15.Scrolpane;
+public class GUI15 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scrolpane brie = new Scrolpane();
+        brie.setVisible(true);
+    }
+}
