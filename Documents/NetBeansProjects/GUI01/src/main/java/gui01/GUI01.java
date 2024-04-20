c9ff94b6874cc468cbc4ef82bb77bef88db3b20c
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 13:50:31 -0500

    saturday

---
+++ Documents/NetBeansProjects/GUI01/src/main/java/gui01/GUI01.java
@@ -0,0 +1,15 @@
+
+
+package gui01;
+
+import javax.swing.JFrame;
+import gui01.CVrified;
+public class GUI01 {
+
+    public static void main(String[] args) {
+        System.out.println("CUADRO VERIFIED");
+        
+        CVrified cuad = new CVrified();
+        cuad.setVisible(true);
+    }
+}
