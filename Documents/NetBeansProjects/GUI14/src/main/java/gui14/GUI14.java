2b016d7c63c86550b8a13b3da940b3819931f97e
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 14:20:43 -0500

    saturday

---
+++ Documents/NetBeansProjects/GUI14/src/main/java/gui14/GUI14.java
@@ -0,0 +1,17 @@
+
+
+package gui14;
+
+import javax.swing.JFrame;
+import gui14.Filechs;
+
+public class GUI14 {
+
+    public static void main(String[] args) {
+        System.out.println("Selector de archivos");
+        
+        Filechs file = new Filechs();
+        
+        file.setVisible(true);
+    }
+}
