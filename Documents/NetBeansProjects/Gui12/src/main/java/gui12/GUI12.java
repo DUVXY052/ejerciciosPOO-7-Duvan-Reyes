47ba3554b007e3e0f0b38ce1fab058918016104b
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 12:19:44 -0500

    friday

---
+++ Documents/NetBeansProjects/Gui12/src/main/java/gui12/GUI12.java
@@ -0,0 +1,11 @@
+
+package gui12;
+
+import javax.swing.JFrame;
+import gui12.Toolsbar;
+public class GUI12 {
+    public static void main(String[] args) {
+        Toolsbar tool= new Toolsbar ();
+        tool.setVisible(true);
+    }
+}
