340f810a9f899656a70353edd0eb37ab42b17497
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-19 22:44:37 -0500

    friday

---
+++ Documents/NetBeansProjects/GUI09/src/main/java/com/mycompany/gui09/GUI09.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.gui09;
+
+import com.mycompany.gui09.Spinner;
+public class GUI09 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Spinner fact = new Spinner();
+        fact.setVisible(true);
+    }
+}
