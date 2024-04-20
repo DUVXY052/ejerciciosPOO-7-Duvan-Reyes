5b36aebae372580a7c4703022eef7b9f78bc284b
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-19 23:24:35 -0500

    friday

---
+++ Documents/NetBeansProjects/comb/src/comb/Comb.java
@@ -0,0 +1,23 @@
+
+
+package comb;
+
+import javax.swing.JFrame;
+import comb.Combs;
+
+/**
+ *
+ * @author USER
+ */
+public class Comb extends JFrame{
+
+    
+    public static void main(String[] args) {
+        
+        Combs combo = new Combs();
+        combo.setVisible(true);
+    
+            
+    }
+    
+}
