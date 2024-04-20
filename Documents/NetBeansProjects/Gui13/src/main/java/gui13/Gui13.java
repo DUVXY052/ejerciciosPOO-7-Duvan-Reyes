0b5782685d25849304311080aeaa32511053ea3a
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 12:48:35 -0500

    friday

---
+++ Documents/NetBeansProjects/Gui13/src/main/java/gui13/Gui13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gui13;
+
+import javax.swing.JFrame;
+import gui13.MousCLK;
+public class Gui13 {
+
+    public static void main(String[] args) {
+        System.out.println("Mouse event");
+        MousCLK mouse= new MousCLK();
+        mouse.setVisible(true);
+    }
+}
