78d963400064d7e07fb7cf93e937a656c80eb69c
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 00:27:48 -0500

    friday

---
+++ Documents/NetBeansProjects/GUI6E/src/main/java/Numerator/moded.java
@@ -0,0 +1,18 @@
+
+
+package Numerator;
+
+import javax.swing.JFrame;
+
+import Numerator.Nums;
+
+
+public class moded extends JFrame{
+    
+    public static void main(String[] args) {
+        Nums num = new Nums();
+        num.setVisible(true);
+    }
+    
+
+}
