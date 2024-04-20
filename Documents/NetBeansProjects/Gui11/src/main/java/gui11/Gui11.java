d17e3476a87e1d2120df8ff10dcbba02759ef484
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-20 01:28:38 -0500

    friday

---
+++ Documents/NetBeansProjects/Gui11/src/main/java/gui11/Gui11.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gui11;
+
+import gui11.MenuDv;
+
+public class Gui11 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        MenuDv menu = new MenuDv ();
+        menu.setVisible(true);
+    }
+}
